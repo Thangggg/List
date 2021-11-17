4811a2fa62e05126e5e3e0132dcb87f4539f1cf7
Thangggg <thang.nguyenvan7594@gmail.com>
2021-11-17 09:02:08 +0700

    TH2

---
+++ src/TH2/Run.java
@@ -0,0 +1,17 @@
+package TH2;
+
+public class Run {
+    public static void main(String[] args) {
+        System.out.println("Tét");
+        MyLinkedList myLinkedList = new MyLinkedList(10);
+        myLinkedList.add(5,1994);
+        myLinkedList.add(1,2000);
+        myLinkedList.add(2,2010);
+
+        myLinkedList.addFirst(7);
+        myLinkedList.get(4);
+
+
+
+    }
+}
